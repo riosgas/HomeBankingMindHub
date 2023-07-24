4d6c8569cd1148f40497a0b123816da9760dbfc4
Gastón Ríos <g.rios@outlook.com>
2023-07-24 18:44:18 -0300

    Add project files.

---
+++ HomeBankingMinHub/Models/DbInitializer.cs
@@ -0,0 +1,33 @@
+﻿using HomeBankingMinHub.Models;
+using System;
+using System.Linq;
+
+namespace HomeBankingMindHub.Models
+{
+    public class DBInitializer
+    {
+        public static void Initialize(HomeBankingContext context)
+        {
+            if (!context.Clients.Any())
+            {
+                var clients = new Client[]
+                {
+                    new Client { FirstName="Victor", LastName="Coronado", Email = "vcoronado@gmail.com", Password="123456"},
+                    new Client { FirstName="Juan", LastName="Carlos", Email = "jc@gmail.com", Password="123456"}
+                };
+
+                foreach (Client client in clients)
+                {
+                    context.Clients.Add(client);
+                }
+                //Client client1 = new Client { FirstName = "Victor", LastName = "Coronado", Email = "vcoronado@gmail.com", Password = "123456" };
+
+                //context.Clients.Add(client1);
+
+                //guardamos
+                context.SaveChanges();
+            }
+
+        }
+    }
+}
---
+++ HomeBankingMinHub/Models/HomeBankingContext.cs
@@ -0,0 +1,16 @@
+﻿using HomeBankingMinHub.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Threading.Tasks;
+namespace HomeBankingMindHub.Models
+{
+    public class HomeBankingContext : DbContext
+    {
+        public HomeBankingContext(DbContextOptions<HomeBankingContext> options) : base(options) {}
+        public DbSet<Client> Clients { get; set; }
+
+    }
+}
