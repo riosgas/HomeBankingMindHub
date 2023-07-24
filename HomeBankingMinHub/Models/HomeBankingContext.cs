a0845d377da6ebba5633581080ce00c8ec289af5
Gastón Ríos <g.rios@outlook.com>
2023-07-24 20:11:58 -0300

    Added Model Account

--- HomeBankingMinHub/Models/HomeBankingContext.cs
+++ HomeBankingMinHub/Models/HomeBankingContext.cs
@@ -11,6 +11,7 @@ namespace HomeBankingMindHub.Models
     {
         public HomeBankingContext(DbContextOptions<HomeBankingContext> options) : base(options) {}
         public DbSet<Client> Clients { get; set; }
+        public DbSet<Account> Accounts { get; set; }
 
     }
 }
